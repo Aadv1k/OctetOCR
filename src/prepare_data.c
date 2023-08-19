9b28b6c9f3fe8319b80767634fd0396fb0125f2e
aadv1k <aadv1k@outlook.com>
2023-08-19 23:47:43 +0530

    Some tests but, But a ground up re-work is required

--- src/prepare_data.c
+++ src/prepare_data.c
@@ -9,109 +9,77 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-OctetData *octet_prepare_training_data_from_dir(const char *dirpath) {
-  DIR *dir;
-  struct dirent *dir_item;
-  if ((dir = opendir(dirpath)) == NULL) {
-    fprintf(stderr, "ERROR: Unable to open directory '%s': %s\n", dirpath, strerror(errno));
-    exit(1);
-  }
+int utils_count_images_in_dir(const char *dirpath) {
+    int count = 0;
+    DIR *dir;
+    struct dirent *entry;
 
-  OctetData *data = malloc(sizeof(OctetData));
-  if (!data) {
-    closedir(dir); // Close the directory before exiting due to memory allocation failure
-    return NULL;
-  }
-  data->characters = NULL;
-  data->characterCount = 0;
-
-  while ((dir_item = readdir(dir)) != NULL) {
-    if (dir_item->d_name[0] == '.') continue;
-
-    char *fileExt = strrchr(dir_item->d_name, '.');
-
-    if (strcmp(fileExt, ".png") != 0 && strcmp(fileExt, ".jpg") != 0 && strcmp(fileExt, ".jpeg") != 0)
-      continue;
-
-    char label = dir_item->d_name[0];
-    char filepath[256];
-    snprintf(filepath, sizeof(filepath), "%s/%s", dirpath, dir_item->d_name);
-
-    int width, height, channels;
-    unsigned char *image_bytes = stbi_load(filepath, &width, &height, &channels, 0);
-    if (!image_bytes) {
-      fprintf(stderr, "WARNING: Unable to load image from '%s', ignoring\n", filepath);
-      continue;
+    dir = opendir(dirpath);
+    if (dir == NULL) {
+        perror("Unable to open directory");
+        return -1;
     }
 
-    if (channels != 1) {
-      octet_convert_rgb_image_to_grayscale(image_bytes, width, height);
-      channels = 1;
+    while ((entry = readdir(dir)) != NULL) {
+        if (entry->d_name[0] == '.') {
+            continue; // Ignore hidden files
+        }
+
+        char *fileExt = strrchr(entry->d_name, '.');
+        if (fileExt != NULL) {
+            if (strcmp(fileExt, ".png") == 0 ||
+                strcmp(fileExt, ".jpg") == 0 ||
+                strcmp(fileExt, ".jpeg") == 0) {
+                count++;
+            }
+        }
     }
 
-    //octet_crop_edges_grayscale(image_bytes, &width, &height);
-    octet_covert_grayscale_image_to_bits(image_bytes, width, height);
-
-    OctetCharacter newCharacter = {
-      .bytes = image_bytes,
-      .label = label,
-      .width = width,
-      .height = height
-    };
-
-    OctetCharacter *temp = realloc(data->characters, (data->characterCount + 1) * sizeof(OctetCharacter));
-    if (!temp) {
-      stbi_image_free(image_bytes); // Free the image_bytes if realloc fails
-      continue;
-    }
-
-    data->characters = temp;
-    data->characters[data->characterCount] = newCharacter;
-    data->characterCount++;
-  }
-
-  closedir(dir);
-  return data;
+    closedir(dir);
+    return count;
 }
 
 void octet_free_data(OctetData *data) {
-  for (size_t i = 0; i < data->characterCount; i++) {
-    stbi_image_free(data->characters[i].bytes);
-  }
-  free(data->characters);
-  free(data);
+    for (int i = 0; i < data->characterCount; i++) {
+        stbi_image_free(data->characters[i].bytes);
+    }
 }
 
 void octet_dump_training_data_to_file(OctetData *data, const char* filepath) {
-  FILE* file = fopen(filepath, "w");
+    FILE* file = fopen(filepath, "w");
 
-  if (file == NULL) {
-    fprintf(stderr, "ERROR: Unable to open file '%s': %s\n", filepath, strerror(errno));
-    exit(1);
-  }
+    if (file == NULL) {
+        fprintf(stderr, "ERROR: Unable to open file '%s': %s\n", filepath, strerror(errno));
+        exit(1);
+    }
 
-  fprintf(file, "total_labels: %d\n\n", data->characterCount);
+    fprintf(file, "total_labels: %d\n\n", data->characterCount);
 
-  for (int i = 0; i < data->characterCount; i++) {
-    fprintf(file, "bytes: %.*s\n", data->characters[i].width * data->characters[i].height, data->characters[i].bytes);
-    fprintf(file, "width: %d\n", data->characters[i].width);
-    fprintf(file, "height: %d\n", data->characters[i].height);
-    fprintf(file, "label: %c\n", data->characters[i].label);
-    fprintf(file, "\n\n");
-  }
+    for (int i = 0; i < data->characterCount; i++) {
+        fprintf(file, "width: %d\n", data->characters[i].width);
+        fprintf(file, "height: %d\n", data->characters[i].height);
+        fprintf(file, "label: %c\n", data->characters[i].label);
 
-  if (fclose(file) != 0) {
-    fprintf(stderr, "ERROR: Unable to close file '%s': %s\n", filepath, strerror(errno));
-    exit(1);
-  }
+        // Convert and write image bytes as hexadecimal
+        fprintf(file, "bytes: ");
+        for (int j = 0; j < data->characters[i].width * data->characters[i].height; j++) {
+            fprintf(file, "%02X", data->characters[i].bytes[j]);
+        }
+        fprintf(file, "\n\n");
+    }
+
+    if (fclose(file) != 0) {
+        fprintf(stderr, "ERROR: Unable to close file '%s': %s\n", filepath, strerror(errno));
+        exit(1);
+    }
 }
 
 
 OctetData *octet_read_training_data_from_file(const char *filename) {
     FILE *file = fopen(filename, "r");
     if (file == NULL) {
-      fprintf(stderr, "ERROR: Unable to open file '%s': %s\n", filename, strerror(errno));
-      exit(1);
+        fprintf(stderr, "ERROR: Unable to open file '%s': %s\n", filename, strerror(errno));
+        return NULL;
     }
 
     OctetData *octetData = (OctetData *)malloc(sizeof(OctetData));
@@ -127,29 +95,130 @@ OctetData *octet_read_training_data_from_file(const char *filename) {
 
     octetData->characters = (OctetCharacter *)malloc(totalLabels * sizeof(OctetCharacter));
 
+    if (!octetData->characters) {
+        fclose(file);
+        free(octetData);
+        return NULL;
+    }
+
     octetData->characterCount = totalLabels;
 
     for (int i = 0; i < totalLabels; i++) {
         OctetCharacter *character = &octetData->characters[i];
 
         int width, height;
-        if (fscanf(file, "bytes: <binary_pixel_data_%*d>\n") != 0 &&
-            fscanf(file, "width: %d\n", &width) == 1 &&
-            fscanf(file, "height: %d\n", &height) == 1 &&
-            fscanf(file, "label: %c\n", &character->label) == 1) {
-            
-            character->bytes = (unsigned char *)malloc(width * height * sizeof(unsigned char));
-            fread(character->bytes, sizeof(unsigned char), width * height, file);
-        } else {
+        if (fscanf(file, "width: %d\n", &width) != 1 ||
+            fscanf(file, "height: %d\n", &height) != 1 ||
+            fscanf(file, "label: %c\n", &character->label) != 1) {
             fprintf(stderr, "ERROR: Corrupted file format, unable to read!\n");
             fclose(file);
-            free(octetData->characters);
-            free(octetData);
+            octet_free_data(octetData); // Free previously allocated data
             return NULL;
         }
+
+        character->width = width;
+        character->height = height;
+
+        // Allocate memory for image bytes
+        character->bytes = (unsigned char *)malloc(width * height);
+        if (!character->bytes) {
+            fclose(file);
+            octet_free_data(octetData); // Free previously allocated data
+            return NULL;
+        }
+
+        // Read and decode hexadecimal bytes
+        for (int j = 0; j < width * height; j++) {
+            unsigned int byte;
+            if (fscanf(file, "%2X", &byte) != 1) {
+                fprintf(stderr, "ERROR: Corrupted file format, unable to read bytes!\n");
+                fclose(file);
+                octet_free_data(octetData); // Free previously allocated data
+                return NULL;
+            }
+            character->bytes[j] = (unsigned char)byte;
+        }
+
+        fscanf(file, "\n"); // Read the newline character after the bytes
     }
 
     fclose(file);
     return octetData;
 }
 
+OctetData *octet_prepare_training_data_from_dir(const char *dirpath) {
+    DIR *dir;
+    struct dirent *dir_item;
+
+    if ((dir = opendir(dirpath)) == NULL) {
+        fprintf(stderr, "ERROR: Unable to open directory '%s': %s\n", dirpath, strerror(errno));
+        exit(1);
+    }
+
+    OctetData *data = (OctetData*)malloc(sizeof(OctetData));
+
+    if (!data) {
+        closedir(dir);
+        return NULL;
+    }
+
+    int itemCount = utils_count_images_in_dir(dirpath);
+    data->characters = (OctetCharacter*)malloc(sizeof(OctetCharacter) * itemCount);
+
+    if (!data->characters) {
+        closedir(dir);
+        free(data);
+        return NULL;
+    }
+
+    data->characterCount = 0;
+
+    while ((dir_item = readdir(dir)) != NULL) {
+        if (dir_item->d_name[0] == '.') continue;
+
+        char *filename = strdup(dir_item->d_name);
+        char *fileExt = strrchr(filename, '.');
+
+        if (strcmp(fileExt, ".png") != 0 && strcmp(fileExt, ".jpg") != 0 && strcmp(fileExt, ".jpeg") != 0) {
+            free(filename);
+            continue;
+        }
+
+        *fileExt = '\0';
+
+        char label = dir_item->d_name[0];
+        char filepath[256];
+        snprintf(filepath, sizeof(filepath), "%s/%s", dirpath, dir_item->d_name);
+
+        int width, height, channels;
+        unsigned char *image_bytes = stbi_load(filepath, &width, &height, &channels, 0);
+        if (!image_bytes) {
+            fprintf(stderr, "WARNING: Unable to load image from '%s', ignoring\n", filepath);
+            free(filename);
+            continue;
+        }
+
+        if (channels != 1) {
+            octet_convert_rgb_image_to_grayscale(image_bytes, width, height);
+            channels = 1;
+        }
+
+        octet_crop_edges_grayscale(image_bytes, &width, &height);
+        //octet_covert_grayscale_image_to_bits(image_bytes, width, height);
+
+        OctetCharacter character = {
+            .bytes = image_bytes,
+            .label = label,
+            .width = width,
+            .height = height
+        };
+
+        data->characters[data->characterCount] = character;
+
+        data->characterCount++;
+        free(filename);
+    }
+
+    closedir(dir);
+    return data;
+}
--- tests/data_read_write_test.c
+++ tests/data_read_write_test.c
@@ -2,28 +2,69 @@
 #include "munit/munit.h"
 
 #define TEST_DATASET_PATH "./dataset/"
+#define TEMP_DATA_PATH "./temp.dat"
 
-static MunitResult isProcessed_fromRawData_toFile(const MunitParameter params[], void* data) {
+static OctetData* trainingData;
+static OctetData* trainingDataB;
+
+static MunitResult PrepareDataFromDir_Dataset_ShouldProcessAndLoad(const MunitParameter params[], void* data) {
     (void)params;
     (void)data;
 
-
-    OctetData* trainingData = octet_prepare_training_data_from_dir(TEST_DATASET_PATH);
-
-
+    trainingData = octet_prepare_training_data_from_dir(TEST_DATASET_PATH);
     munit_assert_int(trainingData->characterCount, ==, 4);
 
     char labels[4] = {'A', 'B', 'C', 'D'};
 
     for (int i = 0; i < trainingData->characterCount; i++) {
-      munit_assert_char(trainingData->characters[i].label, ==, labels[i]);
+        munit_assert_char(trainingData->characters[i].label, ==, labels[i]);
     }
-
-    octet_free_data(data);
     return MUNIT_OK;
 }
 
+static MunitResult WriteDataToFile_OctetData_ShouldWrite(const MunitParameter params[], void* data) {
+    (void)params;
+    (void)data;
+
+    octet_dump_training_data_to_file(trainingData, TEMP_DATA_PATH);
+
+    // You need to add assertions here to check if the TEMP_DATA_PATH was created and exists
+    // Example assertions: munit_assert_int(access(TEMP_DATA_PATH, F_OK), ==, 0);
+
+    return MUNIT_OK;
+}
+
+static MunitResult LoadDataFromFile_TempDataPath_ShouldRead(const MunitParameter params[], void* data) {
+    (void)params;
+    (void)data;
+
+
+    trainingDataB = octet_read_training_data_from_file(TEMP_DATA_PATH);
+    munit_assert_ptr_not_null(trainingDataB);
+
+
+    munit_assert_int(trainingDataB->characterCount, ==, trainingData->characterCount);
+
+    for (int i = 0; i < trainingDataB->characterCount; i++) {
+        munit_assert_char(trainingDataB->characters[i].label, ==, trainingData->characters[i].label);
+        munit_assert_int(trainingDataB->characters[i].width, ==, trainingData->characters[i].width);
+        munit_assert_int(trainingDataB->characters[i].height, ==, trainingData->characters[i].height);
+
+        // Compare image bytes if needed
+    }
+
+    return MUNIT_OK;
+}
+
+static void test_teardown(void* fixture) {
+    (void)fixture;
+    octet_free_data(trainingData);
+    octet_free_data(trainingDataB);
+}
+
 MunitTest dataReadWriteTests[] = {
-  { (char*) "/isProcessed_fromRawData_toFile", isProcessed_fromRawData_toFile, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
+  { (char*) "/PrepareDataFromDir_Dataset_ShouldProcessAndLoad", PrepareDataFromDir_Dataset_ShouldProcessAndLoad, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
+  { (char*) "/WriteDataToFile_OctetData_ShouldWrite", WriteDataToFile_OctetData_ShouldWrite, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
+  { (char*) "/LoadDataFromFile_TempDataPath_ShouldRead", LoadDataFromFile_TempDataPath_ShouldRead, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
   { NULL, NULL, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL }
 };
