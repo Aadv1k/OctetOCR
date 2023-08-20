b9e019a98bf3c8d3e479ad07c86a07260d310aca
aadv1k <aadv1k@outlook.com>
2023-08-20 19:11:31 +0530

    fix the logic for image processing

--- src/image_processing.c
+++ src/image_processing.c
@@ -40,22 +40,11 @@ void octet_threshold_grayscale_image(unsigned char* data, int width, int height,
 
 
 void octet_crop_edges_grayscale(unsigned char* data, int *width, int *height) {
-  // crop the left edge
   for (int x = 0; x < *width; x++) {
     for (int y = 0; y < *height; y++) {
       if (data[y * (*width) + x] != 255) {
         memmove(data, data + x, (*width - x) * (*height));
         *width -= x;
-        break;
-      }
-    }
-  }
-
-  // crop the right edge
-  for (int x = *width - 1; x >= 0; x--) {
-    for (int y = 0; y < *height; y++) {
-      if (data[y * (*width) + x] != 255) {
-        *width = x + 1;
         break;
       }
     }
