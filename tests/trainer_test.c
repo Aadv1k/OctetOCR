19acd42846565396611b43f4d7fd0cb3050433f4
aadv1k <aadv1k@outlook.com>
2023-08-22 11:53:59 +0530

    Add working tests and new test data

--- tests/trainer_test.c
+++ tests/trainer_test.c
@@ -4,6 +4,9 @@
 #define TEST_DATASET_PATH "./dataset/"
 #define TEMP_DATA_PATH "./temp.csv"
 
+#define TEST_A_PATH "./tests/test_data/test-A.jpg"
+#define TEST_A_CURSIVE_PATH "./tests/test_data/test-A-cursive.jpg"
+
 MunitResult Knn_ExactMatch_ShouldCompute(const MunitParameter params[], void* data) {
     (void)params;
     (void)data;
@@ -22,17 +25,39 @@ MunitResult Knn_ExactMatch_ShouldCompute(const MunitParameter params[], void* da
     return MUNIT_OK;
 }
 
-MunitResult Knn_SlightMatch_ShouldCompute(const MunitParameter params[], void* data) {
+MunitResult Knn_SlightMatchesOf_A_ShouldCompute(const MunitParameter params[], void* data) {
     (void)params;
     (void)data;
 
-    return MUNIT_SKIP;
+    OctetData* trainingDataFromMem = octet_load_training_data_from_dir(TEST_DATASET_PATH);
+    munit_assert_not_null(trainingDataFromMem);
+
+    OctetCharacter* character1 = octet_load_character_from_image(TEST_A_PATH);
+    munit_assert_not_null(character1);
+
+    OctetCharacter* character2 = octet_load_character_from_image(TEST_A_CURSIVE_PATH);
+    munit_assert_not_null(character2);
+
+    char predictedLabel = octet_k_nearest_neighbour(*character1, trainingDataFromMem, /* k */ 3);
+    munit_assert_char('A', ==, predictedLabel);
+    
+    char predictedLabel2 = octet_k_nearest_neighbour(*character2, trainingDataFromMem, /* k */ 3);
+    munit_assert_char('A', ==, predictedLabel);
+
+    free(character1->bytes);
+    free(character1);
+
+    free(character2->bytes);
+    free(character2);
+
+    octet_free_training_data(trainingDataFromMem);
+    return MUNIT_OK;
 }
 
 
 
 MunitTest trainerTests[] = {
     { (char*) "/Knn_ExactMatch_ShouldCompute", Knn_ExactMatch_ShouldCompute, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
-    { (char*) "/Knn_SlightMatch_ShouldCompute", Knn_SlightMatch_ShouldCompute, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
+    { (char*) "/Knn_SlightMatchesOf_A_ShouldCompute", Knn_SlightMatchesOf_A_ShouldCompute, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
     { NULL, NULL, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL }
 };
