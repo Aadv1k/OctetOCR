71a6007469c96847611168c3b1a1fc30b79e3c7b
aadv1k <aadv1k@outlook.com>
2023-08-23 11:16:18 +0530

    Add some minor changes to rarafael/octet pull #1

--- build.sh
+++ build.sh
@@ -3,10 +3,13 @@
 CC="cc"
 CFLAGS="-std=c99 -Wall -Wextra -pedantic -I ./include -lm"
 BIN="octet"
+LIB="-lm"
 SRC="$(find ./src -type f -name '*.c' -printf '%h/%f ')\
      $(find ./tests -type f -name '*.c' -printf '%h/%f ')"
 
 if [ "$1" = "TEST" ]; then
     CFLAGS="${CFLAGS} -ggdb"
-    exec ${CC} ${CFLAGS} ${SRC} -o ${BIN}
+    BIN="octet_test"
 fi
+
+exec ${CC} ${CFLAGS} ${LIB} ${SRC} -o ${BIN}
