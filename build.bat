7fd027c1e767f35f15b99589238b64b4562c6fde
aadv1k <aadv1k@outlook.com>
2023-08-23 15:45:34 +0530

    Add command to build shared `.dll`

--- build.bat
+++ build.bat
@@ -12,4 +12,10 @@ IF "%1%" == "TEST" (
   SET CFLAGS=%CLFAGS% -ggdb
 )
 
+IF "%1%" == "LIB" (
+  SET SRC=src\*.c
+  %CC% -std=c99 -ggdb -fPIC -shared -I %INC_PATH% %SRC% -o liboctet.dll
+  exit 0
+)
+
 %CC% %CFLAGS% -I %INC_PATH% %SRC% -o %EXEC%
