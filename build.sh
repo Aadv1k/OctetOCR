9cc5695af57bf7abc72eac68a4d6286fd7896537
Rafael Marian <rafaelmrpch@outlook.com>
2023-08-23 20:32:45 -0300

    added LIB to shell and fixed gitignore
    added a LIB rule to the shell build script and fixed the rest (now it
    has the same behavior as the .bat script).
    Fixed the .gitignore to ignore libraries and the test outputs from Unix.

--- build.sh
+++ build.sh
@@ -2,14 +2,20 @@
 
 CC="gcc"
 CFLAGS="-std=c99 -Wall -Wextra -pedantic -I ./include"
+LDFLAGS="-lm"
 BIN="octet"
-LIB="-lm"
-SRC="$(find ./src -type f -name '*.c' -printf '%h/%f ')" 
+SRC="$(find ./src -type f -name '*.c' -printf '%h/%f ') ./example.c"
 
 if [ "$1" = "TEST" ]; then
     CFLAGS="${CFLAGS} -ggdb"
     BIN="octet_test"
-    SRC+="$(find ./tests -type f -name '*.c' -printf '%h/%f ')"
+    SRC="$(find ./src -type f -name '*.c' -printf '%h/%f ')\
+         $(find ./tests -type f -name '*.c' -printf '%h/%f ')"
 fi
 
-${CC} ${CFLAGS} ${LIB} ${SRC} -o ${BIN}
+if [ "$1" = "LIB" ]; then
+    exec ${CC} ${CFLAGS} ${LDFLAGS} -ggdb -fPIC -shared ${SRC} -o liboctet.so
+    return 0
+fi
+
+exec ${CC} ${CFLAGS} ${LDFLAGS} ${SRC} -o ${BIN}
